39583180b768952d15241857a60b6cbb72131564
Ritik <ritik.k.1404@gmail.com>
2025-03-11 14:24:47 +0530

    Initial Commit

---
+++ kalagato_iOS_Assi/kalagato_iOS_AssiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kalagato_iOS_AssiApp.swift
+//  kalagato_iOS_Assi
+//
+//  Created by Ritik Karma on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct kalagato_iOS_AssiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
